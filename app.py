e803fec4ed9321696d510a937cd7436581707b82
Taka <103984069+Taka0007@users.noreply.github.com>
2023-10-02 03:39:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats
+#from apps import home, data_stats
+from apps import all_songs
 
 app = MultiApp() 
 
-app.add_app("Home", home.app)
-app.add_app("Data Stats", data_stats.app) 
+app.add_app("All_songs",all_songs.app)
+
+#app.add_app("Home", home.app)
+#app.add_app("Data Stats", data_stats.app) 
 
 app.run()
