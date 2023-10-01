cbdf850b6fce71e93800ce1931e75e49e9325c94
Taka <103984069+Taka0007@users.noreply.github.com>
2023-10-02 03:42:55 +0900

    Update all_songs.py

--- apps/all_songs.py
+++ apps/all_songs.py
@@ -6,27 +6,25 @@ import pandas as pd
 import json
 import requests
 
-# GitHubのJSONファイルのURL
-# rowファイルじゃないとダメ
-github_url = "https://raw.githubusercontent.com/Taka0007/song-recommendation-apps/main/data/songs.json"
+def app():
+    # GitHubのJSONファイルのURL (rawファイルを指定)
+    github_url = "https://raw.githubusercontent.com/Taka0007/song-recommendation-apps/main/data/songs.json"
 
-# JSONデータをGitHubから読み込む
-response = requests.get(github_url)
-if response.status_code == 200:
-    karaoke_data = json.loads(response.text)
-else:
-    st.error("JSONデータを読み込めません。GitHub URLを確認してください。")
+    # JSONデータをGitHubから読み込む
+    response = requests.get(github_url)
+    if response.status_code == 200:
+        karaoke_data = json.loads(response.text)
+    else:
+        st.error("JSONデータを読み込めません。GitHub URLを確認してください.")
 
+    # カラオケ曲一覧のページ
+    st.title("カラオケ曲の推薦アプリ")
 
-
-# カラオケ曲一覧のページ
-st.title("カラオケ曲の推薦アプリ")
-
-# カラオケ曲一覧を表示
-st.write("カラオケ曲一覧:")
-for song in karaoke_data["songs"]:
-  st.write(f"曲名: {song['title']}")
-  st.write(f"アーティスト: {song['artist']}")
-  st.write(f"ジャンル: {song['genre']}")
-  st.write(f"評価: {song['rating']}")
-  st.write("---")
+    # カラオケ曲一覧を表示
+    st.write("カラオケ曲一覧:")
+    for song in karaoke_data["songs"]:
+        st.write(f"曲名: {song['title']}")
+        st.write(f"アーティスト: {song['artist']}")
+        st.write(f"ジャンル: {song['genre']}")
+        st.write(f"評価: {song['rating']}")
+        st.write("---")
