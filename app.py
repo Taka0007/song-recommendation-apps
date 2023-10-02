c66384e61ef6f55500fd87fd6bd9a1f63f27ef2f
Taka <103984069+Taka0007@users.noreply.github.com>
2023-10-02 15:18:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from multiapp import MultiApp
 #from apps import home, data_stats
 
 # apps配下のディレクトリにアプリを追加した際には、ここのimportに追加をしないとエラーを吐く
-from apps import all_songs,add_songs
+from apps import all_songs,add_songs,songs_recomendation
 
 app = MultiApp() 
 
 app.add_app("All_songs",all_songs.app)
 app.add_app("Add_songs",add_songs.app)
+app.add_app("Song_recommendation",songs_recomendation.app)
 
 #app.add_app("Home", home.app)
 #app.add_app("Data Stats", data_stats.app) 
