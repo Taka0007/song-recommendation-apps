f5b4b414f695cf1eec7f2eb81bee1b79cfa08e23
Taka <103984069+Taka0007@users.noreply.github.com>
2023-10-02 15:33:16 +0900

    Update songs_recomendation.py

--- apps/songs_recomendation.py
+++ apps/songs_recomendation.py
@@ -5,7 +5,7 @@ import requests
 import streamlit as st
 import numpy as np
 import pandas as pd
-import scikit-learn as sklearn
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
